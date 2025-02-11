498dc0abdd20c3e49db7172afed50b2193b0b150
6410110137 <6410110137@psu.ac.th>
2025-02-11 10:09:40 +0700

    Edit icon

--- iot-monitoring_frontend_react/src/components/Header.js
+++ iot-monitoring_frontend_react/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { MdDashboard } from "react-icons/md";
 
 const HeaderSection = () => (
-    <header className="w-full h-full bg-blue-600 text-white py-4 shadow-md mx-0 md:mx-4  rounded-lg">
+    <header className="w-full h-full bg-blue-600 text-white py-4 shadow-md mx-0 md:mx-4 rounded-lg">
         <div className="container mx-auto flex items-center justify-between">
             {/* Logo Section */}
             <div className="flex items-center mx-4">
                 <div className="bg-white text-blue-600 font-bold text-xl rounded-full w-10 h-10 flex items-center justify-center">
-                    
+                    <MdDashboard size={24} /> 
                 </div>
                 <h1 className="ml-4 text-2xl font-semibold">My Dashboard</h1>
             </div>
